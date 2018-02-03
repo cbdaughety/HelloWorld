37d2980ce8014e8bce697b0602cd587c8b7a6764
cbdaughety <daughetycam@gmail.com>
2018-02-02 20:21:47 -0500

    Initial commit

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+/**
+* Hello World for Git Hub
+* @author: Cameron Daughety
+* @version: 1.0.0 2018.02.02
+*/
+public class HelloWorld{
+	public static void main(String[] args){
+		System.out.println("Hello World!");
+	}
+}
